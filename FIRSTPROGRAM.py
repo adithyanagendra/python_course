edaf9e1fc1fae48aaf09ec4f09979efe19e99561
adithyanagendra <110710218+adithyanagendra@users.noreply.github.com>
2026-02-12 17:44:39 +0530

    Add files via upload

---
+++ FIRSTPROGRAM.py
@@ -0,0 +1,10 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print(10+9)
+19
+>>> print(17-8)
+9
+>>> print(8*7)
+56
+>>> print("hello Adithya")
+hello Adithya
