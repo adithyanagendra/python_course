cc1a0a36cc85a447a0c169632f1e4fb4a63b667d
adithyanagendra <110710218+adithyanagendra@users.noreply.github.com>
2026-02-13 17:28:02 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,93 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+print(4+7)
+11
+a=5
+print(a)
+5
+b=8
+print(b)
+8
+c=50
+print(c)
+50
+y=50
+print(k)
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    print(k)
+NameError: name 'k' is not defined
+a012345678910=500
+print(a012345678910)
+500
+2a=7
+SyntaxError: invalid decimal literal
+a2=41
+print(a2)
+41
+city='vja'
+print(city)
+vja
+name="adithya"
+print(name)
+adithya
+colour="black"
+print(colour)
+black
+print("colour")
+colour
+first_name="adithya"
+print(first_name)
+adithya
+firstname="adithya"
+print(firstname)
+adithya
+@a=450
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> _a=40
+>>> print(_a)
+40
+>>>  b=10
+...  
+SyntaxError: unexpected indent
+>>> a,b=3,4
+>>> print(a+b)
+7
+>>> a=5
+>>> b=8
+>>> print(a+b)
+13
+>>> a=6,b=7
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> a=7;b=10
+>>> print(a+b)
+17
+>>> fname,lname="adithya","g"
+>>> print(fname+lname)
+adithyag
+>>> print(fname+" "+lname)
+adithya g
+>>> print(fname,lname)
+adithya g
+>>> fname="adithya";lname="ch"
+>>> print(fname+lname)
+adithyach
+>>> a=10
+>>> print(a)
+10
+>>> del a
+>>> print(a)
+Traceback (most recent call last):
+  File "<pyshell#47>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined. Did you mean: 'a2'?
+>>> Name="Adithya"
+>>> print(Name)
+Adithya
+>>> Age=27
+>>> print(Age)
+27
+>>> AGE=27
+>>> print(AGE)
+27
