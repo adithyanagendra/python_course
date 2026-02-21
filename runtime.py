6bd2b997c745d28f02fa30458c1debe67b374601
adithyanagendra <110710218+adithyanagendra@users.noreply.github.com>
2026-02-21 17:29:07 +0530

    Add files via upload

---
+++ runtime.py
@@ -0,0 +1,64 @@
+'''a=4
+b=7
+print(a+b)'''
+'''a=2
+b=3
+c=a+b
+print(c)'''
+
+#print(4+9)
+#run_time input()
+#run time input is nothing but givimg the values and input in the compiler
+#int
+'''a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+print(a+b)'''
+'''a=float(input("enter the value of a"))'''
+
+#float
+'''b=float(input("enter the value of b"))
+print(a+b)'''
+
+#str
+'''a=str(input("enter the value of a"))
+b=str(input("enter the value of b"))
+print(a+b)'''
+'''a=(input("enter the value of a"))
+b=(input("enter the value of b"))
+print(a+b)'''
+#complex
+'''a=complex(input("enter the value of a"))
+b=complex(input("enter the value of b"))
+print(a+b)'''
+#boolean
+'''a=bool(input("enter the value of a"))
+b=bool(input("enter the value of b"))
+print(a+b)'''
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input("choose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input(choose the option 1.add
+                                    2.sub
+                                    3.mul))
+print(a+b)
+print(a-b)
+print(a*b)'''
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input("choose the option add sub mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+'''a=input()
+print(a)'''
+
+           
+
