4c3000061751eb083e7e1849aa9a602ae6141018
adithyanagendra <110710218+adithyanagendra@users.noreply.github.com>
2026-02-22 21:35:54 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,18 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> tuple
+<class 'tuple'>
+>>> #tuple
+>>> #tuple()
+>>> #tuple-inmutable
+>>> a=(3,6.5,5+6j,"hi",True,False)
+>>> print(a)
+(3, 6.5, (5+6j), 'hi', True, False)
+>>> type(a)
+<class 'tuple'>
+>>> a.index("hi")
+3
+>>> len(a)
+6
+>>> a.count(True)
+1
