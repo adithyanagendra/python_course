f0f40c4fba6f97a3a1790649423dd649eec36d6a
adithyanagendra <110710218+adithyanagendra@users.noreply.github.com>
2026-02-22 11:25:15 +0530

    Add files via upload

---
+++ revision part1 variables and basic progrmas.py
@@ -0,0 +1,106 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#revision of the basic topics
+#basic program
+print(10+9)
+19
+print(9-5)
+4
+print(9*2)
+18
+print(9%2)
+1
+print(9/2)
+4.5
+#one more example
+#this problem is related to the string ,String is nothing but group of characters.
+print("hi adithya")
+hi adithya
+#variables
+print(4+7)
+11
+a=5
+print(a)
+5
+b=6
+print(b)
+6
+c=50
+print(c)
+50
+k=50
+print(d)
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    print(d)
+NameError: name 'd' is not defined. Did you mean: 'id'?
+#we need to give the variable which is decleard or others it throws error.
+a10234=5
+print(a10234)
+5
+#after declering the variable we can add number in the variable
+2a=5
+SyntaxError: invalid decimal literal
+# we can not declare the number before a variable \
+a2=10
+print(a2)
+10
+city="vij"
+print(city)
+vij
+name="aditya"
+print(name)
+aditya
+print("adithya")
+adithya
+first_name="Adithya"
+print(first_name)
+Adithya
+@a=450
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> #we can't assign a special chracter in front of a variables.
+>>> #but _ is the only special chrackter which can not be assigned.
+>>> _a=40
+>>> print(_a)
+40
+>>>  b=10
+...  
+SyntaxError: unexpected indent
+>>> #we can't give space to the variable.
+>>> a,b=3,4
+>>> print(a+b)
+7
+>>> a=5
+>>> b=6
+>>> print(a+b)
+11
+>>> a=9,b=7
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> a=9;b=10
+>>> print(a+b)
+19
+>>> fname,lname="adithya","g"
+>>> print(fname+lname)
+adithyag
+>>> print(fname+" "+lname)
+adithya g
+>>> print(fname,lname)
+adithya g
+>>> fname="adithya";lname="G"
+>>> print(fname+lname)
+adithyaG
+>>> a=10
+>>> print(a)
+10
+>>> del a
+>>> print(a)
+Traceback (most recent call last):
+  File "<pyshell#59>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined. Did you mean: 'a2'?
+>>> Name="Adithya"
+>>> print(Name)
+Adithya
+>>> Age=27
+>>> print(Age)
+27
