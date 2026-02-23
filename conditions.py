cf223a2fadcebf01323d9bbfdf926909968e8b7c
adithyanagendra <110710218+adithyanagendra@users.noreply.github.com>
2026-02-23 17:54:37 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,180 @@
+#conditions
+#if condition by using comparision operators.
+'''a=2
+b=4
+if a<b:
+    print("true")
+if a>b:
+    print("True")'''
+'''a=10
+b=15
+if a<=b:
+    print("True")'''
+'''a=8
+b=3
+if a>=b:
+    print("True")'''
+'''a=2
+b=4
+if a!=b:
+    print("True")'''
+'''a=10
+b=15
+if a==b:
+    print("True")'''
+'''a=10
+b=10
+if a<=b:
+    print("True")'''
+'''a=2
+b=4
+if a>b:
+    print("True")'''
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a>b:
+    print("greater")'''
+'''a=int(input("a value"))
+if a>40:
+    print("greater")'''
+'''a="python"
+if a=="python":
+    print("true")'''
+'''a=input("name")
+if a=="adithya":
+    print("valid")'''
+#if condition by using logical operators.
+#and or not
+'''a=5
+b=9
+if a<b and b>a:
+    print("less")'''
+'''a=7
+b=11
+if a<=b and b>=a:
+    print("less")'''
+'''a=5
+b=9
+if a!=b and b==a:
+    print("less")'''
+'''a=5
+b=9
+if a<b or b>a:
+    print("less")'''
+'''a=5
+b=9
+if a<=b or b>=a:
+    print("less")'''
+'''a=5
+b=9
+if a!=b or b==a:
+    print("less")'''
+'''a=5
+b=9
+if not a>b:
+        print("less")'''
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<b:
+      print("valid")'''
+ #if-condition by using identify operators
+#is, is not
+'''a=7
+if type(a) is int:
+    print("it is int")'''
+'''a=7
+if type(a) is not int:
+    print("it is int")'''
+'''a=7.5
+if type(a) is not int:
+    print("it is int")'''
+'''a=7.5
+if type(a) is float:
+    print("it is int")'''
+'''a="python"
+if type(a) is str:
+    print("it is a word")'''
+'''a="python"
+if type(a) is not str:
+    print("it is a word")'''
+'''a=6+7j
+if type(a) is complex:
+    print("imaginary no")'''
+'''a=6+7j
+if type(a) is not complex:
+    print("imaginary no")'''
+'''a=True
+if type(a) is bool:
+    print("bool")'''
+'''a=False
+if type(a) is not bool:
+    print("bool")'''
+'''a=int(input("value a"))
+if type(a) is int:
+    print("it is int")'''
+'''a=int(input("value a"))
+if type(a) is not int:
+    print("it is int")'''
+#if_condition by using membership operators
+#in, not in
+'''a=[2,3,4,5,6,7,8,9,10]
+if 2 in a:
+    print("true")'''
+'''a=[2,3,4,5,6,7,8,9,10]
+if 15 not in a:
+    print("true")'''
+'''a=[10,20,30,40,50]
+b=int(input("value"))
+if b in a:
+    print("valid")'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    
+
+
+
+
