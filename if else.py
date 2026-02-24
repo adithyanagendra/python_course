2456ce5a612334aa1d88e495e5a65e6dc0ae3dda
adithyanagendra <110710218+adithyanagendra@users.noreply.github.com>
2026-02-24 18:00:19 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,173 @@
+#if-else conditions by using comprision oparators
+'''a=6
+b=8
+if a<b:
+    print("less")'''
+'''a=6
+b=8
+if a>b:
+    print("less")
+else:
+    print("true")'''
+'''a=6
+b=8
+if a!=b:
+    print("not equal")
+else:
+    print("equal")'''
+'''a=6
+b=8
+if a==b:
+    print("equal")
+else:
+    print("not equal")'''
+'''a=int(input())
+b=int(input())
+if a>b:
+    print("greater")
+else:
+    print("true")'''
+'''a=int(input())
+b=int(input())
+if a>b:
+    print("greater")
+else:
+    print("true")'''
+#if-else conditions by using logical operators
+'''a=6
+b=8
+if a<b and b>a:
+    print("true")
+else:
+    print("false")'''
+'''a=6
+b=8
+if a!=b and b==a:
+    print("eqaul")
+else:
+    print("false")'''
+'''a=6
+b=8
+if not b>a:
+    print("false")
+else:
+    print("true")'''
+'''a=int(input())
+b=int(input())
+if a<b and a>b:
+    print("true")
+else:
+    print("false")'''
+#if-else conditions by using identify operators   is and is not
+'''a=7
+if type(a) is int:
+    print("yes")
+else:
+    print("no")'''
+'''a=8.5
+if type(a) is not int:
+    print("yes")
+else:
+    print("no")'''
+'''a=int(input())
+if type(a) is int:
+    print("yes")
+else:
+    print("no")'''
+#if-else conditions by using memebership operators   in not in
+'''a=[7,8,9,10,11,12,13,14]
+if 7 in a:
+    print("true")
+else:
+    print("false")'''
+'''a=[7,8,9,10,11,12,13,14]
+if 15 not in a:
+    print("true")
+else:
+    print("false")'''
+'''a=[7,8,9,10,11,12,13]
+b=int(input())
+if b in a:
+    print("true")
+else:
+    print("false")'''
+#examples in indentify operators
+'''a=9
+b=10
+if not b<a:
+    print("false")
+else:
+    print("true")'''
+'''a=float(input())
+if type(a) is not float:
+    print("true")
+else:
+    print("false")'''
+#if-elif-else using comparision operators
+'''a=8
+b=12
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")
+else:
+    print("true")'''
+
+'''a=8
+b=12
+if a>b:
+    print("less")
+elif b>a:
+    print("greater")
+else:
+    print("true")'''
+
+'''a=8
+b=12
+if a==b:
+    print("less")
+elif b<a:
+    print("greater")
+elif b>a:
+      print("yes")
+else:
+    print("true")'''
+#if-elif-else conditions by using logical operators
+'''a=6
+b=8
+if a<b and b>a:
+    print("true")
+elif a<b or b>a:
+    print("yes")
+elif not a<b or b>a:
+    print("no")
+else:
+    print("false")'''
+#if-elif-else conditions by using identity operators
+'''a=7.5
+if type(a) is int:
+    print("yes")
+elif type(a) is not int:
+    print("value")
+else:
+    print("no")'''
+#if-elif-else conditions by using membership operators
+'''a=[7,8,9,10,11,12,13,14]
+if 7 in a:
+    print("true")
+elif 7 not in a:
+    print("yes")
+else:
+    print("false")'''
+
+
+
+
+
+    
+
+
+
+
+
+    
