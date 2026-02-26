b9682a22b26fc093692d036bb5d224cf63116282
adithyanagendra <110710218+adithyanagendra@users.noreply.github.com>
2026-02-26 17:30:45 +0530

    Add files via upload

---
+++ multiple and nested if conditions.py
@@ -0,0 +1,116 @@
+#multiple if using comparision operators
+'''a=5
+b=10
+if a<b:
+    print("less")
+if b>a:
+    print("greater")
+if a!=b:
+    print("not equal")'''
+'''a=5
+b=10
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")
+elif a!=b:
+    print("not equal")'''
+'''a=5
+b=10
+if a==b:
+    print("less")
+if b>a:
+    print("greater")
+if a!=b:
+    print("not equal")'''
+'''a=5
+b=10
+if a<b:
+    print("less")
+if b>a:
+    print("greater")
+if a!=b:
+    print("not equal")
+elif a!=b:
+    print("true")'''
+#multiple if using logical operators
+'''a=5
+b=10
+if a<b and b>a:
+    print("less")
+if a<b or a>b:
+    print("greater")
+if not a!=0 :
+    print("not equal")'''
+#multiple if using identity operators
+'''a=5
+b=10
+if type(a) is int:
+    print("yes a is int")
+if type(b) is int:
+    print("yes b is int")
+else:
+    print("false")'''
+#multiple if using membership operators
+'''a=[2,3,4,5,6,7,8,9]
+if 3 in a:
+    print("yes 2 in a")
+if 4 in a:
+    print("yes 4 in a")
+if 5 in a:
+    print("yes 5 in a")
+if 10 not in a:
+    print("yes no in a")'''
+#nested if using condition operators
+
+'''a=10
+b=20
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")'''
+'''a=10
+b=20
+if a==b:
+    print("less")
+    if b>a:
+        print("greater")'''
+'''a=10
+b=20
+if a<=b:
+    print("less")
+    if b==a:
+        print("greater")
+    else:
+        print("true")'''
+'''a=10
+b=20
+if a==b:
+    print("less")
+    if b!=a:
+        print("greater")
+else:
+    print("true")'''
+'''a=10
+b=20
+if a<=b:
+    print("less")
+    if b==a:
+        print("greater")
+    else:
+            print("true")
+else:
+    print("false")'''
+'''a=10
+b=20
+if a<=b:
+    print("less")
+    if b==a:
+        print("greater")
+    elif a==b:
+        print("not eqaul")
+    else:
+        print("true")'''
+
+
+
