38d7b7c75622faf38427ffb79772f4d76d793fe1
adithyanagendra <110710218+adithyanagendra@users.noreply.github.com>
2026-02-28 08:26:41 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,24 @@
+# question voting system above18 is eligible below18 not eligigble
+'''a=int(input("value of a"))
+if a>=18:
+      print("eligible for voting")
+elif a<18:
+    print("not eligible for voting")  
+else:
+    print("no")'''
+#even or odd
+'''a=int(input("enter the value of a"))
+if a%2==0:
+      print("it is even",a)
+elif a%1==0:
+    print("it is odd",a)
+else:
+    print("no")'''
+#vowels
+a=
+#leap year
+year=int(input("enter the year"))
+if year%4==0:
+    print("it is leap year")
+else:
+    print("not leap year")
---
+++ task2.py
@@ -0,0 +1,45 @@
+'''a=input("name of a")
+if a=="adithya":
+    print("welcome",a)
+else:
+    print("welcome guest")'''
+'''a=["adithya","nagendra","satya","vani","krishna"]
+b=input("name of b")
+if b in a:
+    print("welcome",b)
+else:
+    print("welcome guest")'''
+#social media login
+'''username=input("username:")
+password=input("password:")
+if username=="adithya":
+    print("valid username,enter password")
+    if password=="adithya@4560":
+        print("valid")
+    else:
+        print("invalid")
+else:
+    print("not valid username")'''
+#cakes problem
+'''a={"redvelvet":1000,"chocolate":800,"butterscotch":600,"chocoalmond":500}
+b=input("flavour of a")
+if b in a:
+    print("yes available",b)
+else:
+    print("sorry cake is not available")# this is not a correct way'''
+#correct way
+'''cake_price=int(input("enter the value"))
+if cake_price==1200:
+                 print("redvelevt cake")
+elif cake_price==1000:
+    print("chocolate")               
+elif cake_price==800:
+    print("butterscotch")
+else:
+    print("sorry cake is not available")'''
+    
+               
+             
+            
+
+
