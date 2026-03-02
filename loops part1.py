acc338fa5ddae261304eadf53c98b35a22b0fdec
adithyanagendra <110710218+adithyanagendra@users.noreply.github.com>
2026-03-02 20:23:56 +0530

    Add files via upload

---
+++ loops part1.py
@@ -0,0 +1,118 @@
+#loops()
+#for(),while(),range(),break,continue,pass
+#for():sequence iteration
+#list using for()
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)'''
+'''a=[10,20,30,40,50]
+for i in a:
+    print(a)'''
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,"end")'''
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,end="")'''
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,end=",")'''
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+'''a=[4,5,6,7,8,9]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+#sets() method using for()
+'''a={4,5,6,7,8,9}
+for i in a:
+    print(i)'''
+'''a={10,20,30,40,50}
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+#tuple method using for()
+'''a=(4,5,6,7,8,9)
+for i in a:
+    print(i)'''
+'''a=(4,5,6,7,8,9)
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+#dict method using for()
+'''a={"name":"adithya","year":2026,"month":3}
+for i in a:
+    print(i)
+for i in a.keys():
+    print(i)
+    print(type(a))
+    print(type(i))
+for i in a.values():
+    print(i)
+    print(type(a))
+    print(type(i))
+for i in a.items():
+    print(i)'''
+#strings using for()
+'''a="codegnan"
+for i in a:
+    print(i,end=" ")
+    print(type(a))
+    print(type(i))'''
+#for() in real time
+'''a=["code","python","course"]
+b=str(a)
+print(b.upper())'''
+#method 2 using loop
+'''a=["code","python","course"]
+for i in a:
+    print(i.upper(),end=" ")'''
+#while():continues iteration
+'''a=10
+while a>1:
+    print(a)'''
+'''a=10
+while a<1:
+    print(a)'''
+'''a=20
+while a>1:
+    print(a)
+    a=a-1'''
+'''a=20
+while a>=1:
+    print(a)
+    a=a-1'''
+'''a=20
+while a>1:
+    a=a-1
+    print(a)'''
+'''a=20
+while a>1:
+    a=a-1
+print(a)'''
+'''a=15
+while a>2:
+    print(a)
+    a+=1'''
+'''a=15
+while a>=2:
+      print(a)
+      a+=1'''
+'''a=3
+while a<18:
+    print(a)
+    a*=1'''
+
+    
+
+    
+
+
+
