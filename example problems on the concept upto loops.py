2455f28448b951bc7d2f6f7cb058f78a30d2a0dc
adithyanagendra <110710218+adithyanagendra@users.noreply.github.com>
2026-03-05 12:30:24 +0530

    Add files via upload

---
+++ example problems on the concept upto loops.py
@@ -0,0 +1,61 @@
+#tables
+'''a=int(input("enter the value of a"))
+for i in range(0,21):
+      print(f"{a}x{i}={a*i}")'''
+'''a=int(input("enter the value of a"))
+for i in range(0,21):
+    print(a,"x",i,"=",a*i)'''
+
+#student deatils
+'''student_name=input("enter the name")
+mobile_number=int(input("enter the number"))
+maild_id=input("enter the mailid")
+college_name=input("enter the clg name")
+branch=input("enter the branch")
+print("student profile.................")
+print("..................................")
+print("name is",student_name)
+print("number is",mobile_number)
+print("mailid is",maild_id)
+print("clg",college_name)
+print("branch is",branch)'''
+#marks analyzer report
+'''n=int(input("enter the student count"))
+marks=[]
+for i in range(n):
+    mark=int(input("enter the marks"))
+    marks.append(mark)
+for j in marks:
+    print(j)
+heighest_marks=max(marks)
+lowest_marks=min(marks)
+total_marks=sum(marks)
+average=total_marks/n
+print("maximum",heighest_marks)
+print("minimum",lowest_marks)
+print("total",total_marks)
+print("average",average)'''
+#hostel fee+course fee
+'''h=int(input("enter the hostel fee"))
+c=int(input("enter the course fee"))
+print("summary..................")
+print("..........................")
+print("total fee=",h+c)'''
+#method2
+'''h=int(input("enter the hostel fee"))
+c=int(input("enter the course fee"))
+print("hostel fee is=",h)
+print("course fee is=",c)
+print("summary..................")
+print("..........................")
+print("total fee=",h+c)'''
+#patterns-1
+'''rows=5
+for i in range(1,rows+1):
+    print("*"*i)'''
+#patterns-2
+'''rows=5
+for i in range(rows,0,-1):
+    print("*"*i)'''
+
+
