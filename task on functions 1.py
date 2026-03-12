fe2166b5ba8a13899e86f47f04f25ee980a8c938
adithyanagendra <110710218+adithyanagendra@users.noreply.github.com>
2026-03-12 11:35:43 +0530

    Add files via upload

---
+++ task on functions 1.py
@@ -0,0 +1,54 @@
+'''def split_bill():
+    members = 10
+    per_head = 1000
+    total = members * per_head
+
+    print("Members:", members)
+    print("Per Head Amount:", per_head)
+    print("Total Bill:", total)
+    
+split_bill()'''
+
+
+
+'''def split_bill():
+    members = 10
+    per_head = 1000
+    total = members * per_head
+
+    print(f"Members: {members}")
+    print(f"Per Head Amount: {per_head}")
+    print(f"Total Bill: {total}")
+
+split_bill()'''
+
+
+'''def splitbill():
+    a=int(input("enter the total no.of friends"))
+    b=int(input("enter the amount"))
+    print("the bill is",b//a)
+splitbill()'''
+
+
+
+'''def splitbill():
+    a=int(input("enter the total no.of friends"))
+    b=int(input("enter the amount"))
+    c=b//a
+    print("perhead bill is {}".format(c))
+    print(f"the bill is {c}")
+splitbill()'''
+
+
+'''def splitbill():
+    a=int(input("enter the total no.of friends"))
+    b=int(input("enter the amount"))
+    print("perhead bill is {}".format(b//a))
+    print(f"the bill is {b//a}")
+splitbill()'''
+
+
+
+
+
+
