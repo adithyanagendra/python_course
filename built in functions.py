c784732ba0bbdf1a62bbc3aa60432012ae7366d9
adithyanagendra <110710218+adithyanagendra@users.noreply.github.com>
2026-03-12 11:37:15 +0530

    Add files via upload

---
+++ built in functions.py
@@ -0,0 +1,44 @@
+'''print(max(4,5,6,7,8,9,10))
+
+
+print(min(4,5,6,7,8,9,10))
+
+
+#print(sum(4,5,6,7,8,9,10))  it is error
+a=2,3,4,5,6,7,8
+print(sum(a))
+
+print(sum([3,4,5,6,7,8,9]))'''
+
+
+#task
+#no of student in class question
+n=int(input("enter the no of students"))
+a=[]
+count=0
+for i in range(n):
+      s=input("enter student status")
+      a.append(s)
+
+    
+          
+
+
+
+
+'''def student_attendance():
+    total_students = int(input("enter total number of students: "))
+    absent_students = int(input("enter number of absent students: "))
+    
+    present_students = total_students - absent_students
+    
+    print("Total Students:", total_students)
+    print("Absent Students:", absent_students)
+    print("Present Students:", present_students)
+student_attendance()'''
+
+          
+     
+
+
+      
